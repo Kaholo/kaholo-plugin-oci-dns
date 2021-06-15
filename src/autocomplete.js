0e4cf8657a6ef58b5ae6ed6337ea1b6e1df127a0
Amit Alon <amit@kaholo.io>
2021-06-15 17:44:38 +0300

    small update

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -11,13 +11,13 @@ function mapSettings(pluginSettings){
 
 async function listCompartments(query, pluginSettings) {
   const settings = mapSettings(pluginSettings);
-  const rootCompartment = settings.compartment;
+  const tenancyId = settings.tenancyId;
   const provider = getProvider(settings);
   const identityClient = await new identity.IdentityClient({
     authenticationDetailsProvider: provider
   });
   const request = {
-    compartmentId: rootCompartment
+    compartmentId: tenancyId
   };
   const response = await identityClient.listCompartments(request);
   let options = response.items.map((item) => ({ id: item.id, value: item.name}));
